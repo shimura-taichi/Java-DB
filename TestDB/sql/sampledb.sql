5d96e4f17ae630569772fca8d324c0d8f4e215c8
shimura-taichi <155545427+shimura-taichi@users.noreply.github.com>
2024-03-25 21:48:01 +0900

    Add files via upload

--- TestDB/sql/sampledb.sql
+++ TestDB/sql/sampledb.sql
@@ -1,12 +1,8 @@
 drop database if exists sampledb;
-create datebase sampledb;
+create database sampledb;
 use sampledb;
 
-create table sample_table(
-No int, name varchar(50),
-age int,
-Mail varchar(50)
-);
+create table sample_table(No int,name varchar(50),age int,Mail varchar(50));
 
 insert into sample_table values(1,"Yamada",21,"xxx@yahoo.co.jp");
 insert into sample_table values(2,"Sato",39,"yyy@yahoo.co.jp");
